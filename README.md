2132591bb38fbc091374480407684b5995d19e0c
Shripad13 <stmaddi@outlook.com>
2025-12-14 20:14:45 +0530

    updated the file

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # expense-helm
 expense-helm
+
+# You can download & Install the latest Argo CD version by running below commands- 
+Reference - https://argo-cd.readthedocs.io/en/stable/cli_installation/
+'''
+$ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+$ sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+$ rm argocd-linux-amd64
+$ type argocd
+'''
+
+## ArgoCD can be accessed by using CLI & UI
+1. Cluster should be created.
+2. Metrics server should be installed.
+3. Argocd should be installed.
+4. Terraform should get the argocd passwrod dynamically.
+5. Should be programmatically able to sign into argocd
+6. ArgoCD jobs should also be created automatically.
+7. 
---
+++ init.sh
@@ -0,0 +1,15 @@
+#Below command to get ArgoCD URL from k8s service
+ARGO_URL=${kubectl get svc -n argocd|grep argocd-server |grep LoadBalancer | awk '{print $4}'}
+
+#Terraform should get secrets dynamically in json format (from below command) (-o json - in json format)-
+ARGO_PWD=${kubectl get secret argocd-initial-admin-secret -n argocd-initial-admin-secret -o json -n argocd | jq'.data.password' | xargs | base64 -d}
+
+# Login to Argocd in CLI (--insecure-> means it will not validate cert & verification)
+argocd login $ARGO_URL --username admin --password $ARGO_PWD  --insecure
+
+# Create a argocd app
+echo "Creating Dev  Backend app"
+arcgocd app create backend --repo https://github.com/Shripad13/expense-helm.git --path .  --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/backend.yaml
+
+echo "Creating Dev Frontend app"
+arcgocd app create frontend --repo https://github.com/Shripad13/expense-helm.git --path .  --dest-namespace default --dest-server https://kubernetes.default.svc --values dev/frontend.yaml
