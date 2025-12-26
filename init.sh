60d14292766438530ca19b537c113798878f7f31
Shripad13 <stmaddi@outlook.com>
2025-12-26 13:51:42 +0530

    update the file

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #Below command to get ArgoCD URL from k8s service
-ARGO_URL=${kubectl get svc -n argocd|grep argocd-server |grep LoadBalancer | awk '{print $4}'}
+#ARGO_URL=${kubectl get svc -n argocd|grep argocd-server |grep LoadBalancer | awk '{print $4}'}
+
+# Below URL is CNAME record created in Route53 for ArgoCD server LoadBalancer
+ARGO_URL="https://argocd.devsecopswithshri.site"
 
 #Terraform should get secrets dynamically in json format (from below command) (-o json - in json format)-
 ARGO_PWD=${kubectl get secret argocd-initial-admin-secret -n argocd-initial-admin-secret -o json -n argocd | jq'.data.password' | xargs | base64 -d}
